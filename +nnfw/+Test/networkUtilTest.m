2cb107b9114d5ba154b07867c7597657b7d8d0bd
Markus Monz <markusmonz@googlemail.com>
2014-10-29 18:05:46 +0100

    implemented function wrapper for fminunc compatibility
    also implemented a new helper function setWeights, which is needed by
    the fminunc. Each iteration a new weight vector is passed to the network
    and the setWeights function maps the weights from the vector to the
    different layer weight matrizes.
    Added tests for helper function setWeights.

--- +nnfw/+Test/networkUtilTest.m
+++ +nnfw/+Test/networkUtilTest.m
@@ -105,6 +105,93 @@ classdef networkUtilTest < matlab.unittest.TestCase
 
             tc.assertEqual(net.getWeightVector(), expected);
         end
+        
+        function setWeights_01(tc)
+            % --------------------------------------
+            % init network, 1-2-1 nn framework
+            % --------------------------------------
+            net = nnfw.FeedForward(1,2,1);
+            net.inputs{1}.size = 1;
+            net.layers{1}.size = 2;
+            net.outputs{2}.size = 1;
+            
+            weights = rand(7,1);
+            net.setWeights(weights);
+            
+            tc.assertEqual(net.IW{1}, weights(1:2,1));
+            tc.assertEqual(net.b{1}, weights(3:4,1));
+            tc.assertEqual(net.LW{2}, weights(5:6,1)');
+            tc.assertEqual(net.b{2}, weights(7:7,1));
+        end
+        
+        function setWeights_02(tc)
+            % --------------------------------------
+            % init network, 13-2-1 nn framework
+            % --------------------------------------
+            net = nnfw.FeedForward(1,2,1);
+            net.inputs{1}.size = 13;
+            net.layers{1}.size = 2;
+            net.outputs{2}.size = 1;
+            
+            weights = rand(31,1);
+            net.setWeights(weights);
+
+            % assert LW{1} dimensions first
+            R = net.inputs{1}.size;
+            S_1 = net.layers{1}.size;
+            tc.assertEqual(size(net.IW{1}), [S_1 R]);
+            % assert LW{1} weights
+            tc.assertEqual(net.IW{1}(1,:), weights(1:13,1)');
+            tc.assertEqual(net.IW{1}(2,:), weights(14:26,1)');
+            tc.assertEqual(net.b{1}, weights(27:28,1));
+            
+            % assert LW{2} dimensions first
+            S_1 = net.layers{1}.size;
+            S_2 = net.outputs{2}.size;
+            tc.assertEqual(size(net.LW{2}), [S_2 S_1]);
+            % assert LW{2} weights
+            tc.assertEqual(net.LW{2}, weights(29:30,1)');
+            tc.assertEqual(net.b{2}, weights(31));
+        end
+        
+        function setWeights_03(tc)
+            % --------------------------------------
+            % init network, 1-3-2-1 nn framework
+            % --------------------------------------
+            net = nnfw.FeedForward(1,3,1);
+            net.inputs{1}.size = 1;
+            net.layers{1}.size = 3;
+            net.layers{2}.size = 2;
+            net.outputs{3}.size = 1;
+            
+            weights = rand(17,1);
+            net.setWeights(weights);
+            
+            % assert LW{1} dimensions first
+            R = net.inputs{1}.size;
+            S_1 = net.layers{1}.size;
+            tc.assertEqual(size(net.IW{1}), [S_1 R]);
+            % assert LW{1} weights
+            tc.assertEqual(net.IW{1}, weights(1:3,1));
+            tc.assertEqual(net.b{1}, weights(4:6,1));
+            
+            % assert LW{2} dimensions first
+            S_1 = net.layers{1}.size;
+            S_2 = net.layers{2}.size;
+            tc.assertEqual(size(net.LW{2}), [S_2 S_1]);
+            % assert LW{2} weights
+            tc.assertEqual(net.LW{2}(1,:), weights(7:9,1)');
+            tc.assertEqual(net.LW{2}(2,:), weights(10:12,1)');
+            tc.assertEqual(net.b{2}, weights(13:14));
+            
+            % assert LW{3} dimensions first
+            S_2 = net.layers{2}.size;
+            S_3 = net.outputs{3}.size;
+            tc.assertEqual(size(net.LW{3}), [S_3 S_2]);
+            % assert LW{3} weights
+            tc.assertEqual(net.LW{3}, weights(15:16,1)');
+            tc.assertEqual(net.b{3}, weights(17));
+        end
     end
     
 end
--- +nnfw/@FeedForward/FeedForward.m
+++ +nnfw/@FeedForward/FeedForward.m
@@ -39,6 +39,14 @@ classdef FeedForward < nnfw.Network
             % configure network layer sizes
             configure(net, input, target);
             
+            costFcn = net.makeCostFcn(@nnfw.Util.mse, input, target);
+            
+            options = optimoptions('fminunc','GradObj','on', 'PlotFcns', {@optimplotfval, @optimplotstepsize}, 'MaxFunEvals', 30);
+            [x,y,exitFlag,output] = fminunc(costFcn,net.getWeightVector(),options);
+            
+            % set network weights found by optimization function
+            net.setWeights(x);
+            
             % forward propagate with current weights
             % neuron outputs needed for backpropagation will be stored in a
             [y, a] = simulate(net, input);
@@ -269,7 +277,7 @@ classdef FeedForward < nnfw.Network
                     % layer weights
                     startDim = offset +1;
                     endDim = net.inputs{layer}.size * net.layers{layer}.size;
-                    weightVector(startDim:endDim,1) = weightVector(startDim:endDim,1) + net.IW{layer};
+                    weightVector(startDim:endDim,1) = weightVector(startDim:endDim,1) + net.IW{layer}(:);
                     offset = endDim;
                     % bias weights
                     startDim = offset +1;
@@ -280,7 +288,7 @@ classdef FeedForward < nnfw.Network
                     % layer weights
                     startDim = offset +1;
                     endDim = offset + net.layers{layer-1}.size;
-                    weightVector(startDim:endDim,1) = weightVector(startDim:endDim,1) + net.LW{layer}';
+                    weightVector(startDim:endDim,1) = weightVector(startDim:endDim,1) + net.LW{layer}(:);
                     offset = endDim;
                     % bias weights
                     startDim = offset +1;
@@ -291,7 +299,7 @@ classdef FeedForward < nnfw.Network
                     % layer weights
                     startDim = offset +1;                    
                     endDim = offset + net.layers{layer-1}.size * net.layers{layer}.size;
-                    weightVector(startDim:endDim,1) = weightVector(startDim:endDim,1) + net.LW{layer};
+                    weightVector(startDim:endDim,1) = weightVector(startDim:endDim,1) + net.LW{layer}(:);
                     offset = endDim;
                     % bias weights
                     startDim = offset +1;
@@ -300,6 +308,60 @@ classdef FeedForward < nnfw.Network
                     offset = endDim;
                 end
             end
+        end
+        
+        function setWeights(net, weights)
+            offset = 0;
+            for layer = 1:net.numLayers
+                if layer == 1
+                    % layer weights
+                    R = net.inputs{layer}.size;
+                    S_1 = net.layers{layer}.size;
+                    net.IW{layer} = zeros(S_1, R);
+                    for k = 1:S_1
+                        startDim = offset + 1;
+                        endDim = offset + R;
+                        % set weights in k-th row of input weights matrix
+                        % of layer-th layer
+                        net.IW{layer}(k,:) = weights(startDim:endDim,1);
+                        offset = endDim;
+                    end
+                    % bias weights
+                    startDim = offset +1;
+                    endDim = offset + net.layers{layer}.size; 
+                    net.b{layer} = weights(startDim:endDim,1);
+                    offset = endDim;
+                elseif layer == net.numLayers
+                    % layer weights
+                    startDim = offset +1;
+                    endDim = offset + net.layers{layer-1}.size;
+                    net.LW{layer} = weights(startDim:endDim,1)';
+                    offset = endDim;
+                    % bias weights
+                    startDim = offset +1;
+                    endDim = offset + net.outputs{layer}.size;
+                    net.b{layer} = weights(startDim:endDim,1);
+                    offset = endDim;                    
+                else
+                    % layer weights
+                    S_n = net.layers{layer-1}.size; % S_n = size of layer before current layer
+                    S_m = net.layers{layer}.size; % S_m = size of current layer
+                    net.LW{layer} = zeros(S_m, S_n);                    
+                    for k = 1:S_m
+                        startDim = offset + 1;
+                        endDim = offset + S_n;
+                        % set weights in k-th row of input weights matrix
+                        % of layer-th layer
+                        net.LW{layer}(k,:) = weights(startDim:endDim,1);
+                        offset = endDim;
+                    end
+                    % bias weights
+                    startDim = offset +1;
+                    endDim = offset + net.layers{layer}.size; 
+                    net.b{layer} = weights(startDim:endDim,1);
+                    offset = endDim;
+                end
+            end            
         end
     end
     
