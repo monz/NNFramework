db3dc0d8183b7f253b5c887e328f4ffffe5ddc95
Markus Monz <markusmonz@googlemail.com>
2014-11-20 18:33:05 +0100

    now the random numbers have uniform distribution
    reference http://de.mathworks.com/help/matlab/math/floating-point-numbers-within-specific-range.html

--- +nnfw/+Util/getRangedRandomWeights.m
+++ +nnfw/+Util/getRangedRandomWeights.m
@@ -1,21 +1,9 @@
 function weights = getRangedRandomWeights( numWeights, range )
 %GETRANGEDRANDOMWEIGHTS Summary of this function goes here
 %   Detailed explanation goes here
-
-    maxAbsValue = max(abs(range));
     maxValue = max(range);
     minValue = min(range);
-
-    weights = zeros(numWeights, 1);
-    for k = 1:numWeights
-        randValue = (rand()*2 - 1) * maxAbsValue;
-        
-        if randValue < minValue
-            randValue = randValue + abs(maxAbsValue - minValue);
-        elseif randValue > maxValue
-            randValue = randValue - abs(maxAbsValue - maxValue);
-        end        
-        weights(k) = randValue;
-    end
+    
+    weights = (maxValue - minValue).*rand(numWeights,1) + minValue;
 end
 
