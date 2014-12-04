2594d27fcda4aa6ed28bcca572bb3efc600db71c
Markus Monz <markusmonz@googlemail.com>
2014-12-04 19:18:06 +0100

    improved calculation of jacobian matrix

--- +nnfw/@Network/makeCostFcn2.m
+++ +nnfw/@Network/makeCostFcn2.m
@@ -19,8 +19,9 @@ function costFcn = makeCostFcn2(net, fcn, input, target)
             J = zeros(Q*s_MSize, net.getNumWeights());
         end
         % cost function
-        F = bsxfun(@minus,target,y); % for performance enhancement
-        netSize = net.numLayers; % for performance enhancement
+        F = bsxfun(@minus,target,y); % for performance improvement
+        % load often used variables only once for performance improvements
+        netSize = net.numLayers; % for performance improvement
         for q = 1:Q
             if nargout > 1   % Two output arguments
                 % calculate marquardt sensitivity of last layer
@@ -51,48 +52,32 @@ function costFcn = makeCostFcn2(net, fcn, input, target)
                         if ( layer == 1 )
                             jEntriesWeights = s_m{q, layer}(:, outputNr) * input(:, q)';
                             jEntriesBias = s_m{q, layer}(:, outputNr);
-                            layerSize = net.inputs{layer}.size;
-                            layerP1Size = net.layers{layer}.size;
                         elseif ( layer == netSize )
                             s_MVector = zeros(s_MSize, 1);
                             s_MVector(outputNr) = s_M(outputNr,q);
                             jEntriesWeights = s_MVector * a{q, layer-1}';
                             jEntriesBias = s_MVector;
-                            layerSize = net.layers{layer-1}.size;
-                            layerP1Size = net.outputs{layer}.size;
                         else
                             jEntriesWeights = s_m{q, layer}(:, outputNr) * a{q, layer-1}';
                             jEntriesBias = s_m{q, layer}(:, outputNr);
-                            layerSize = net.layers{layer-1}.size;
-                            layerP1Size = net.layers{layer}.size;
                         end
                         % prepare jacobian entries to be saved in a row of
                         % jacobian matrix
-                        jEntriesWeights2 = zeros(1, layerSize*layerP1Size);
-                        offset2 = 0;
-                        for k = 1:layerP1Size
-                            startDim2 = offset2 +1;
-                            endDim2 = offset2 + layerSize;
-                            jEntriesWeights2(1, startDim2:endDim2) = jEntriesWeights2(1, startDim2:endDim2) + jEntriesWeights(k,:); % error derived at weights
-                            offset2 = endDim2;
-                        end
-                        jEntriesWeights = jEntriesWeights2;
+                        jEntriesWeights = reshape(jEntriesWeights',[1, numel(jEntriesWeights)]);
                         jEntriesBias = jEntriesBias(:)'; % error derived at bias
                         % save jacobian entries to the q-th jacobian matrix row
                         % jEntries of weights
                         startDim = offset+1;
-                        endDim = offset+length(jEntriesWeights);
+                        offset = offset+length(jEntriesWeights);
                         
                         rowIdx = (q-1)*s_MSize + (outputNr);
-                        J(rowIdx, startDim:endDim) = J(rowIdx, startDim:endDim) + jEntriesWeights;                
+                        J(rowIdx, startDim:offset) = J(rowIdx, startDim:offset) + jEntriesWeights;
                         
-                        offset = endDim;
                         % jEntries of biases
                         startDim = offset+1;
-                        endDim = offset + length(jEntriesBias);
+                        offset = offset + length(jEntriesBias);
                         
-                        J(rowIdx, startDim:endDim) = J(rowIdx, startDim:endDim) + jEntriesBias;                
-                        offset = endDim;                
+                        J(rowIdx, startDim:offset) = J(rowIdx, startDim:offset) + jEntriesBias;
                     end 
                 end
             end
