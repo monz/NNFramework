c86801f95616152f452259ac3501be7013b8dbe8
Markus Monz <markusmonz@googlemail.com>
2015-01-25 00:18:08 +0100

    clean up

--- Untersuchungen/SignalTransformation/prepareDataBlock.m
+++ Untersuchungen/SignalTransformation/prepareDataBlock.m
@@ -3,10 +3,7 @@ function [ p, t, teData, exData ] = prepareDataBlock( input, target, testData, e
 %   Detailed explanation goes here
 
     numInputs = size(input,1);
-    inputDataLength = size(input,2);
     numTargets = size(target,1);
-    numTests = size(testData,1);
-    numExtra = size(extraData,1);
     % repead target values until numInputs and numTargets are equal
     t = zeros(numInputs,length(target));
     if numTargets < numInputs
@@ -24,16 +21,6 @@ function [ p, t, teData, exData ] = prepareDataBlock( input, target, testData, e
     else
         t = target;
     end
-
-%     % reshape to one row for SISO trainig
-%     dimensionInput = numInputs*inputDataLength;
-%     dimensionTests = numTests*inputDataLength;
-%     dimensionExtra = numExtra*inputDataLength;
-%     p = reshape(input', 1, dimensionInput);
-%     t = reshape(t', 1, dimensionInput);
-%     teData = reshape(testData', 1, dimensionTests);
-%     exData = reshape(extraData', 1, dimensionExtra);
-
     
     p = input';
     t  = t';
