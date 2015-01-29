ae70a62e01a98d661a30b83e7e878ffce803cf76
Markus Monz <markusmonz@googlemail.com>
2015-01-29 20:33:01 +0100

    added documentation to Network class

--- +nnfw/@Network/Network.m
+++ +nnfw/@Network/Network.m
@@ -1,6 +1,14 @@
 classdef (Abstract) Network < handle
-    %NET Summary of this class goes here
-    %   Detailed explanation goes here
+    %NETWORK Contains common attributes of all neural network types.
+    %   NETWORK is a abstract class, hence no objects of type NETWORK can
+    %   be created. Instead create objects of one of the subclasses. E.g.
+    %   FeedForward. It implements a special version of the NETWORK class.
+    %   The NETWORK class extends the handle class. This means if a network
+    %   object (or subclass of it) is passed to a function, this function can
+    %   change the attributes directly, because the object is not copied on
+    %   function call.
+    %
+    %   See also HANDLE, FEEDFORWARD.
     
     properties
         isPatternNet = false; % if true, exit node converts values in range [0...1]
@@ -25,23 +33,26 @@ classdef (Abstract) Network < handle
         layerConnect = [] % currently unused
         outputConnect = [] % currently unused
         
-        IW = {} % input weights for each input vector
+        IW = {} % input weights for each input layer
         LW = {} % layer weights for each hidden layer
         b = [] % biases
         
     end
     
     methods
-        initNetwork(obj, numInputs, numLayers, numOutputs);
+        initNetwork(obj, numInputs, numLayers, numOutputs); % initializes the network layers with default values
         initWeights(obj) % set random layer weights for train purpose
         costFcn = makeCostFcn(net, fcn, input, target); % for fminunc
         costFcn = makeCostFcn2(net, fcn, input, target); % for lsqnonlin
         
         function obj = Network()
+            % Initializes the default optimization values on object
+            % creation
             initOptimValues(obj)
         end
-        % initialize optimazation values with defaults
+        
         function initOptimValues(obj) 
+            % initialize optimazation values with defaults
             obj.optim.plotFcns = {};
         
             obj.optim.vlFactor = 0.20; % validation data factor, default 20 percent of input data are used for validation
@@ -58,13 +69,20 @@ classdef (Abstract) Network < handle
     end
     
     methods (Abstract)
+        % The implementation should configure the network layer sizes properly.
         configure(obj, varargin);
+        % The implementation should simulate the values through the network while considering the network characteristics.
         simulate(obj, varargin);
+        % The implementation should train the adapt the network weights while considering the network characteristics.
         train(obj, input, target);
+        % The implementation should return the total number of weight values in the neural network.
         getNumWeights(obj);
+        % The implementation should return the gradient value related to the given weight value.
         getGradientByWeight(obj, gradVector, weight);
+        % The implementation should return all weight values in a single vector
         getWeightVector(obj);
-        setWeights(obj);
+        % The implementation should replace the networks weight vector with the giben weight vector
+        setWeights(obj, weights);
     end
     
 end
