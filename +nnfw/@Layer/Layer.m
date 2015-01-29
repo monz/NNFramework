5d864f482a2137a97234e86411401febe0f09d1c
Markus Monz <markusmonz@googlemail.com>
2015-01-29 18:25:45 +0100

    added documentation to class Layer in nnfw

--- +nnfw/@Layer/Layer.m
+++ +nnfw/@Layer/Layer.m
@@ -1,15 +1,19 @@
 classdef Layer < handle
-    %LAYER Summary of this class goes here
-    %   Detailed explanation goes here
+    %LAYER Describes the layers of a neural network
+    %   Each neuron has a activation function. In this implementation all
+    %   neurons in the same layer have the same activation function f. The
+    %   type of the network is used for clearness only. The size describes
+    %   the total number of neurons contained in a layer.
     
     properties
-        type % input, hidden, output
-        f % activation function
+        type % can be one of {'input', 'hidden', 'output'}
+        f % activation function handle
         size % number of layer neurons
     end
     
     methods
         function obj = Layer(layerType, transferFnc)
+            % Creates a layer with specified type and activation function
             obj.type = layerType;
             obj.f = transferFnc;
         end
